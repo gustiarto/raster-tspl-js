63c4bb2a181a1181d6c7f272e9734d6352204086
gustiarto <gusti.arto@gmail.com>
2022-02-16 22:24:33 +0700

    write serial tspl

--- index.js
+++ index.js
@@ -28,3 +28,19 @@ jimp.read(labelPath, async (err, image) => {
         // console.log({ byteData, img, row, byte });
       }
     }
+
+    const label = Buffer.concat([
+        Buffer.alloc(512),
+        Buffer.from("\r\nSIZE 99.8 mm, 149.9 mm"),
+        Buffer.from("\r\nSET TEAR ON"),
+        Buffer.from("\r\nSET CUTTER OFF"),
+        Buffer.from("\r\nSET PEEL OFF"),
+        Buffer.from("\r\nCLS"),
+        Buffer.from("\r\nBITMAP 0,0,100,1198,1,"),
+        img,
+        Buffer.from("\r\nPRINT 1,1"),
+        Buffer.from("\r\n"),
+      ]);
+    // fs.writeFileSync("out.tspl", label); // debugging
+    fs.writeFileSync("/dev/usb/lp0", label);
+});
